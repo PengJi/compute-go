84c4858830cbdf18ab99001fd9e74aede447e3a5
pengji <jipengpro@gmail.com>
2023-09-17 15:34:10 +0800

    modify example

--- example/eg3-rs/src/main.rs
+++ example/eg3-rs/src/main.rs
@@ -1,6 +1,4 @@
-use kvm_bindings::{
-    kvm_userspace_memory_region, KVM_MAX_CPUID_ENTRIES, KVM_MEM_LOG_DIRTY_PAGES,
-};
+use kvm_bindings::{kvm_userspace_memory_region, KVM_MAX_CPUID_ENTRIES, KVM_MEM_LOG_DIRTY_PAGES};
 use kvm_ioctls::Kvm;
 use vm_memory::{GuestAddress, GuestMemoryMmap};
 
